871b9dd849c4cf6e3bdfd8ca1964794eb9708e34
Elise Phan <aliceconnic@gmail.com>
2019-01-10 22:50:41 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -31,7 +31,7 @@ attr_reader :id
       VALUES (?, ?)
     SQL
    DB[:conn].execute(sql, self.name, self.grade)
-   @id = DB[:conn].execute("SELECT last_insert_rowid FROM students")
+   @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")
   end
 
 end
