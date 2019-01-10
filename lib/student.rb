9909c24d42453c6a7dc29b489e8bda926a6698f5
Elise Phan <aliceconnic@gmail.com>
2019-01-10 22:47:00 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -28,7 +28,7 @@ attr_reader :id
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade)
-      VALUEs (?,?)
+      VALUES (?,?)
     SQL
    DB[:conn].execute(sql, self.name, self.grade)
   end
