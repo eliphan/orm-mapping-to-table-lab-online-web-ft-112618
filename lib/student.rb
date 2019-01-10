2e3340b589a4d1d47aa146d808f967030e518363
Elise Phan <aliceconnic@gmail.com>
2019-01-10 22:38:14 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,5 +10,14 @@ attr_reader :id
     @name = name
     @grade = grade
   end
+  
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS students (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade TEXT
+      )
+    SQL
  
 end
